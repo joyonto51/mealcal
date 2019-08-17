f4ee8ccc525231237f55eae4a21ef9194a1e0625
Jayanta Roy <joyonto51@gmail.com>
2019-08-17 13:53:44 +0600

    Stable Version 0.1

--- app/src/main/java/com/itsd/mealcal/Main2Activity.kt
+++ app/src/main/java/com/itsd/mealcal/Main2Activity.kt
@@ -1,9 +1,7 @@
 package com.itsd.mealcal
 
-import android.net.ParseException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main2.*
@@ -21,50 +19,134 @@ class Main2Activity : AppCompatActivity() {
 
         val meal_rate = intent.getStringExtra("meal_rate")
         val total_cost = intent.getStringExtra("total_cost")
-        val fine_range = intent.getStringExtra("fine_range")
-        val fine_rate = intent.getStringExtra("fine_rate")
+
+        val has_fine = intent.getBooleanExtra("has_fine", false)
         val countGuestMeal = intent.getBooleanExtra("count_guest_meal", false)
 
+        if (has_fine){
+            val fine_range = intent.getStringExtra("fine_range")
+            val fine_rate = intent.getStringExtra("fine_rate")
+
+            fineRangeTxt.text = getString(R.string.fineRangeText, fine_range)
+            fineRateTxt.text = getString(R.string.fineRateText, fine_rate)
+
+            fineRateTxt.visibility = View.VISIBLE
+            fineRangeTxt.visibility = View.VISIBLE
+
+//            headerLayout.maxHeight = 50
+
+            fineRange = fine_range!!.toInt()
+            fineRate = fine_rate!!.toFloat()
+
+        }
+
         mealRatetxt.text = getString(R.string.mealRateText, meal_rate)
-        totalCostTxt.text = getString(R.string.totalCostText, total_cost)
-        fineRangeTxt.text = getString(R.string.fineRangeText, fine_range)
-        fineRateTxt.text = getString(R.string.fineRateText, fine_rate)
+        totalCostTxt.text = getString(R.string.extraCostText, total_cost)
 
         mealRate = meal_rate!!.toFloat()
         totalCost = total_cost!!.toInt()
-        fineRange = fine_range!!.toInt()
-        fineRate = fine_rate!!.toFloat()
 
-        println("=====>>> ${mealRate}, ${totalCost}, ${fineRange}, ${fineRate}")
+        if (countGuestMeal){
+            countGuestMealText.visibility = View.VISIBLE
+            guestMealTk.visibility = View.VISIBLE
+        }
 
         calculate.setOnClickListener {
+            var guestMeal = 0
+
             try {
                 val totalBalance = totalBalance.text.toString().toInt()
                 val totalMeal = totalMeal.text.toString().toInt()
-                val guestMeal = guestMealTk.text.toString().toInt()
 
-
-                val totalSpend = totalCost + (mealRate * totalMeal) + guestMeal
-                val remainingBlance = totalBalance - totalSpend
-                var fine = 0
-                if (remainingBlance < 0 && (remainingBlance*-1) >= fineRange ){
-                    fine = ((remainingBlance/fineRange).toInt() * fineRate.toInt()) + -1
+                if (countGuestMeal){
+                    if(guestMealTk.text.isNotEmpty()){
+                        guestMeal = guestMealTk.text.toString().toInt()
+                        calculate(totalBalance, totalMeal, guestMeal)
+                    }
+                    else {
+                        makeOutputUnVisible()
+                        makeFineOutputUnVisivble()
+                        toast("Please, give guest meal charge")
+                    }
+                }
+                else{
+                    calculate(totalBalance, totalMeal, guestMeal)
                 }
 
-                val totalDue = remainingBlance + fine
-
-                println("=====>> ${totalSpend}, ${remainingBlance}, ${fine}, ${totalDue}")
-
-                totalSpendBalance.text = getString(R.string.total_spend, totalSpend.toString())
-                remainingBalance.text = getString(R.string.remaining_balance, remainingBlance.toString())
-                fineBalance.text = getString(R.string.fine_balance, fine.toString())
-                dueBalance.text = getString(R.string.due_balance, totalDue.toString())
             }
            catch (e:NumberFormatException){
-               Toast.makeText(this, "Please, give all input", Toast.LENGTH_SHORT).show()
+               makeOutputUnVisible()
+               makeFineOutputUnVisivble()
+               toast("Please, give total balance & meal")
            }
 
         }
 
+    }
+
+    fun calculate(totalBalance:Int, totalMeal: Int, guestMeal: Int){
+        var fine = 0F
+
+        // ============================ Calculation ============================ \\
+
+        val totalSpend = totalCost + (mealRate * totalMeal) + guestMeal
+        val remainingBlance = totalBalance - totalSpend
+
+        if (remainingBlance < 0 && (remainingBlance*-1) >= fineRange ){
+            fine = ((remainingBlance/fineRange).toInt() * fineRate) * -1
+        }
+
+        val totalDue = remainingBlance*-1 + fine
+
+        // ============================      End     =========================== \\
+
+
+        totalSpendBalance.text = totalSpend.toString()
+        remainingBalance.text = remainingBlance.toString()
+
+        makeOutputVisible()
+
+        if (fine > 0){
+            fineBalance.text = fine.toString()
+            dueBalance.text = totalDue.toString()
+
+            makeFineOutputVisivble()
+        }
+        else{
+            makeFineOutputUnVisivble()
+        }
+    }
+
+    fun toast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+    }
+
+    fun makeOutputVisible(){
+        totalSpendBalance.visibility = View.VISIBLE
+        remainingBalance.visibility = View.VISIBLE
+        totalSpendBalanceText.visibility = View.VISIBLE
+        remainingBalanceText.visibility = View.VISIBLE
+    }
+
+    fun makeOutputUnVisible(){
+        totalSpendBalance.visibility = View.INVISIBLE
+        remainingBalance.visibility = View.INVISIBLE
+        totalSpendBalanceText.visibility = View.INVISIBLE
+        remainingBalanceText.visibility = View.INVISIBLE
+    }
+
+    fun makeFineOutputVisivble(){
+        fineBalance.visibility = View.VISIBLE
+        dueBalance.visibility = View.VISIBLE
+        fineBalanceText.visibility = View.VISIBLE
+        dueBalanceText.visibility = View.VISIBLE
+    }
+
+    fun makeFineOutputUnVisivble(){
+        fineBalanceText.visibility = View.INVISIBLE
+        dueBalanceText.visibility = View.INVISIBLE
+        fineBalance.visibility = View.INVISIBLE
+        dueBalance.visibility = View.INVISIBLE
     }
 }
--- app/src/main/java/com/itsd/mealcal/MainActivity.kt
+++ app/src/main/java/com/itsd/mealcal/MainActivity.kt
@@ -3,6 +3,7 @@ package com.itsd.mealcal
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,30 +13,61 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        hasFine.setOnClickListener {
+            if (hasFine.isChecked){
+                fineRangeText.visibility = View.VISIBLE
+                fineRange.visibility = View.VISIBLE
+                fineRateText.visibility = View.VISIBLE
+                fineRate.visibility = View.VISIBLE
+            }
+            else{
+                fineRangeText.visibility = View.INVISIBLE
+                fineRange.visibility = View.INVISIBLE
+                fineRateText.visibility = View.INVISIBLE
+                fineRate.visibility = View.INVISIBLE
+            }
+        }
+
         gobtn.setOnClickListener {
             val mealRate = mealRate.text
-            val totalCost = totalCost.text
+            val extraCost = extraCost.text
             val fineRange = fineRange.text
             val fineRate = fineRate.text
 
-            if(mealRate.isEmpty() || totalCost.isEmpty() || fineRange.isEmpty() || fineRate.isEmpty()){
-                Toast.makeText(this, "Please, give all input.", Toast.LENGTH_SHORT).show()
+            if(mealRate.isEmpty() || extraCost.isEmpty()){
+                toast("Please, give meal rate & extra cost")
             }
+
+            else if (hasFine.isChecked && (fineRange.isEmpty() || fineRate.isEmpty())){
+                    toast("Please, give fine range & fine rate.")
+                }
+
             else{
-                println("=====>>> ${mealRate}, ${totalCost}, ${fineRange}, ${fineRate}")
+                println("=====>>> ${mealRate}, ${extraCost}, ${fineRange}, ${fineRate}")
 
                 val countGuestMeal = guestMealCheckBox.isChecked
+                val hasFine = hasFine.isChecked
 
                 val SecondIntent = Intent(this, Main2Activity::class.java)
 
                 SecondIntent.putExtra("meal_rate", mealRate.toString())
-                SecondIntent.putExtra("total_cost", totalCost.toString())
-                SecondIntent.putExtra("fine_range", fineRange.toString())
-                SecondIntent.putExtra("fine_rate", fineRate.toString())
+                SecondIntent.putExtra("total_cost", extraCost.toString())
+
+                SecondIntent.putExtra("has_fine", hasFine)
                 SecondIntent.putExtra("count_guest_meal", countGuestMeal)
+
+                if(hasFine){
+                    SecondIntent.putExtra("fine_range", fineRange.toString())
+                    SecondIntent.putExtra("fine_rate", fineRate.toString())
+                }
 
                 startActivity(SecondIntent)
             }
         }
+    }
+
+    fun toast(message:String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
     }
 }
