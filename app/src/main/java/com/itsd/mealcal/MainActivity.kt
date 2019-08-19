00f1cba1918fad0b520eba310d07ae3a81b3ab16
Jayanta Roy <joyonto51@gmail.com>
2019-08-19 22:25:34 +0600

    About Activity added and logo updated

--- app/src/main/java/com/itsd/mealcal/MainActivity.kt
+++ app/src/main/java/com/itsd/mealcal/MainActivity.kt
@@ -3,15 +3,42 @@ package com.itsd.mealcal
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.first_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        onClickListener()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.about -> {
+                startActivity(Intent(this, AboutActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    fun onClickListener(){
 
         hasFine.setOnClickListener {
             if (hasFine.isChecked){
@@ -39,8 +66,8 @@ class MainActivity : AppCompatActivity() {
             }
 
             else if (hasFine.isChecked && (fineRange.isEmpty() || fineRate.isEmpty())){
-                    toast("Please, give fine range & fine rate.")
-                }
+                toast("Please, give fine range & fine rate.")
+            }
 
             else{
                 println("=====>>> ${mealRate}, ${extraCost}, ${fineRange}, ${fineRate}")
